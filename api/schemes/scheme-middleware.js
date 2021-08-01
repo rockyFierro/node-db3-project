54a674b41d072cf21d585d4ea83fcde7b0f3350d
Rocky Fierro <39335873+rockyFierro@users.noreply.github.com>
2021-08-01 12:53:02 -0700

    middleware draft finished

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,4 @@
+const database = require('../../data/db-config');
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,8 +7,23 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const scheme = await
+      database('schemes')
+        .where('scheme_id', req.params.scheme_id)
+        .first();
+    if (!scheme) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.id} not found`
+      });
+    } else {
+      next(res.json(scheme));
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -19,8 +35,20 @@ const checkSchemeId = (req, res, next) => {
   }
 */
 const validateScheme = (req, res, next) => {
-
-}
+  const { scheme_name } = req.body;
+  if (
+    scheme_name === undefined ||
+    typeof scheme_name !== 'string' ||
+    !scheme_name.trim()
+  ) {
+    next({
+      status: 400,
+      message: "invalid scheme_name"
+    });
+  } else {
+    next();
+  }
+};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -32,8 +60,24 @@ const validateScheme = (req, res, next) => {
   }
 */
 const validateStep = (req, res, next) => {
+  const { instructions, step_number } = req.body;
 
-}
+  if (
+    instructions === undefined ||
+    typeof instructions !== 'string' ||
+    !instructions.trim() ||
+    typeof step_number !== 'number' ||
+    step_number < 1
+  ) {
+    const error = {
+      status: 400,
+      message: 'invalid step'
+    };
+    next(error);
+  } else {
+    next();
+  }
+};
 
 module.exports = {
   checkSchemeId,
--- api/schemes/scheme-router.js
+++ api/schemes/scheme-router.js
@@ -1,6 +1,6 @@
 // DO NOT CHANGE THIS FILE
 const express = require('express')
-const { checkSchemeId, validateScheme, validateStep } = require('./scheme-middleware')
+const { checkSchemeId, validateScheme, validateStep } = require('./scheme-middleware');
 const Schemes = require('./scheme-model.js')
 
 const router = express.Router()
@@ -26,9 +26,9 @@ const router = express.Router()
 router.get('/', (req, res, next) => {
   Schemes.find()
     .then(schemes => {
-      res.json(schemes)
+      res.json(schemes);
     })
-    .catch(next)
+    .catch(next);
 })
 
 /*
@@ -53,13 +53,13 @@ router.get('/', (req, res, next) => {
   }
 */
 router.get('/:scheme_id', checkSchemeId, (req, res, next) => {
-  const { scheme_id } = req.params
+  const { scheme_id } = req.params.id;
 
   Schemes.findById(scheme_id)
     .then(scheme => {
-      res.json(scheme)
+      res.json(scheme);
     })
-    .catch(next)
+    .catch(next);
 })
 
 /*
@@ -101,14 +101,14 @@ router.get('/:scheme_id/steps', checkSchemeId, (req, res, next) => {
   }
 */
 router.post('/', validateScheme, (req, res, next) => {
-  const scheme = req.body
+  const scheme = req.body;
 
   Schemes.add(scheme)
     .then(scheme => {
-      res.status(201).json(scheme)
+      res.status(201).json(scheme);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 /*
   [POST] /api/schemes/5/steps { "instructions": "and yet more questing", "step_number": 2 }
@@ -130,22 +130,22 @@ router.post('/', validateScheme, (req, res, next) => {
   ]
 */
 router.post('/:scheme_id/steps', checkSchemeId, validateStep, (req, res, next) => {
-  const step = req.body
-  const { scheme_id } = req.params
+  const step = req.body;
+  const { scheme_id } = req.params;
 
   Schemes.addStep(scheme_id, step)
     .then(allSteps => {
-      res.status(201).json(allSteps)
+      res.status(201).json(allSteps);
     })
-    .catch(next)
-})
+    .catch(next);
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
   res.status(err.status || 500).json({
     sageAdvice: 'Finding the real error is 90% of the bug fix',
     message: err.message,
     stack: err.stack,
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;
